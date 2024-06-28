34fbbf7becba9cca43ed555509f30f500ab1d167
prijall <khadkaprijal@gmail.com>
2024-06-28 22:42:16 +0545

    Data Retreving

---
+++ Main.py
@@ -0,0 +1,22 @@
+from zipfile import ZipFile
+import os 
+import urllib
+import urllib.request
+
+
+#@ Import Dataset from Website:
+URL='https://nnfs.io/datasets/fashion_mnist_images.zip'
+FILE='fashion_mnist_images.zip'
+FOLDER= 'fashion_mnist_images'
+
+
+if not os.path.isfile(FILE):
+    print(f'Downloading {URL} and Saving as {FILE}...')
+    urllib.request.urlretrieve(URL, FILE)
+
+print('Unzipping images....')
+
+with ZipFile(FILE) as zip_images:
+    zip_images.extractall(FOLDER)
+
+print('Done!')

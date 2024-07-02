b1c5a48e2ef8ae80e9d94af99b71bb359d20f896
prijall <khadkaprijal@gmail.com>
2024-07-02 20:10:11 +0545

    Added activation(all kinds)

--- Source_Code.py
+++ Source_Code.py
@@ -74,4 +74,78 @@ class Dropout_Layer:
         self.dinputs=dvalues*self.binary_mask       
 
         
- 
+ #@ Input Layer:
+class Input_Layer:
+    #forward pass:
+    def forward(self, inputs):
+        self.output=inputs
+
+#@ ReLU Activation:
+class ReLU_Activation:
+
+    #forward pass:
+    def forward(self, inputs):
+        self.inputs=inputs
+        self.output=np.maximum(0, inputs)
+
+    #Backward Pass:
+    def backward(self, dvalues):
+        #since we intend to modify the original values, we need to make a copy:
+        self.dinputs=dvalues.copy()
+        self.dinputs[self.inputs<=0]=0
+
+
+#@ Softmax Activaiton:
+class Softmax_activation:
+
+    #forward pass:
+    def forward(self, inputs):
+        self.inputs= inputs
+        exp_values=np.exp(inputs-np.max(inputs, axis=1, keepdims=True))
+        probabilities=exp_values/np.sum(exp_values, axis=1, keepdims=True)
+        self.output=probabilities
+
+     #Backward Pass:
+    def backward(self, dvalues):
+        self.dinputs=np.empty_like(dvalues)
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
+            single_output=single_output.reshape(-1, 1)
+            jacobian_matrix=np.diagflat(single_output)- np.dot(single_output, single_output.T)
+            self.dinputs[index]=np.dot(jacobian_matrix, single_dvalues)
+     
+      #prediction for output:
+    def prediction(self, outputs):
+        return np.argmax(outputs, axis=1)
+
+#@ Sigmoid Activation:
+class Sigmoid_Activation:
+
+    #forward pass:
+    def forward(self, inputs):
+        self.inputs=inputs
+        self.output=1/(1+np.exp(-inputs))
+
+    #Backward Pass:
+    def backward(self, dvalues):
+        self.dinputs=dvalues*(1-self.output)*self.output
+
+    #prediction for output:
+    def prediction(self, outputs):
+        return (outputs>0.5)*1
+    
+
+#@ Linear Activation:
+class Linear_Activation:
+    #forward pass:
+    def forward(self, inputs):
+        self.inputs=inputs
+        self.output=inputs
+
+    # Backward Pass:
+    def backward(self, dvalues):
+        self.dinputs=dvalues.copy()
+
+    #prediction for output:
+    def predictions(self, outputs):
+        return outputs
+    

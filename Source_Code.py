d31a8d80f20afbab3cf47f96d750a933df88ea05
prijall <khadkaprijal@gmail.com>
2024-07-07 22:17:16 +0545

    Added accuracy function

--- Source_Code.py
+++ Source_Code.py
@@ -251,3 +251,49 @@ class Loss:
     def new_pass(self):
         self.accumulated_sum=0
         self.accumulated_count=0
+
+
+#@ for accuracy:
+class Accuracy:
+    def calculate(self, predictions, y):
+        comparisons=self.compare(predictions, y)
+        accuracy=np.mean(comparisons)
+
+        #adding acculumated sum of matching values and sample count:
+        self.accumulated_sum+= np.sum(comparisons)
+        self.accumulated_count+= len(comparisons)
+
+        return accuracy
+    
+    #for accumulated accuracy:
+    def calculate_accumulated(self):
+        accuracy=self.accumulated_sum / self.accumulated_count
+        return accuracy
+    
+    #reseting variable for accumulated accuracy:
+    def new_pass(self):
+        self.accumulated_sum=0 
+        self.accumulated_count=0
+
+
+#@ Accuracy for classification model:
+class Accuracy_Categorical(Accuracy):
+    def init(self, y):
+        pass
+
+    def compare(self, predictions, y):
+        if len(y.shape)==2:
+            y=np.argmax(y, axis=1)
+        return predictions==y
+    
+#@ Accuracy for regression model:
+class Accuracy_Regression(Accuracy):
+    def __init__(self):
+        self.precision=None
+
+    def init(self, y, reinit=False):
+        if self.precision is None or reinit:
+            self.precision=np.std(y)/ 250
+
+    def compare(self, predictions, y):
+        return np.absolute(predictions-y)<self.precision
